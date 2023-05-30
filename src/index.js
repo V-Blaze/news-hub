40a8b6b5ccd4ae9c954c0b7ac8a4d5d52e409017
V-Blaze <valentine6586@gmail.com>
2023-05-30 15:29:53 +0100

    Add React router DOM to project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+// Redux
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// StyleSheet
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
