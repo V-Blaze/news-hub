dfe9872dd593f6b29a3709252628a420efc5b974
V-Blaze <valentine6586@gmail.com>
2023-05-31 17:49:07 +0100

    Add proper styling to the hero section card component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,49 +1,86 @@
-import React from 'react'
+import React from 'react';
+
+// Componenets
+import PopularNewsCard from './PopularNewsCard';
+
+// Assets
+import { heroIMG } from '../assets/images';
 
 const Hero = () => {
+  const PopularNews = [
+    {
+      category: 'Science',
+      time: '2 Hours Ago',
+      slug: '7 Ways You Can Reduce Climate Change',
+      image: heroIMG,
+    },
+    {
+      category: 'Inovation',
+      time: '4 Hours Ago',
+      slug: '7 Ways You Can Reduce Climate Change',
+      image: heroIMG,
+    },
+    {
+      category: 'Tech',
+      time: '1 Hours Ago',
+      slug: '7 Ways You Can Reduce Climate Change',
+      image: heroIMG,
+    },
+    {
+      category: 'Science',
+      time: '1 Mins Ago',
+      slug: '7 Ways You Can Reduce Climate Change',
+      image: heroIMG,
+    },
+    {
+      category: 'Science',
+      time: '6 Mins Ago',
+      slug: '7 Ways You Can Reduce Climate Change',
+      image: heroIMG,
+    },
+  ];
+
   return (
-    
-    <section>
-      <div>
-        <div>
-          <span>inovation</span>
+
+    <section className="grid grid-cols-3 w-full p-8 bg-slate-400 gap-4">
+      <div className="flex flex-col gap-5 col-span-2 text-white">
+        <div className="text-white flex gap-4">
+          <span>Inovation</span>
           <span>2hrs ago</span>
         </div>
-        <div>
-          <h1>
-          Charge Two Devices at the Same Time With This Magnetic Wireless Charging Dock
+        <div className=" w-full">
+          <h1 className="font-semibold font-serif text-7xl w-[80%]">
+            Charge Two Devices at the Same Time With This Magnetic Wireless Charging Dock
           </h1>
         </div>
-        <div>
-          <div>
-            <img src="/" alt="news" />
-            <p>Charge Two Devices at the Same Time With This Magnetic Wireless Charging Dock
+        <div className="grid grid-cols-2 gap-3">
+          <div className="flex flex-col gap-2 border-2 border-l-4 border-[#366BD9] rounded-lg p-3 hover:bg-slate-500">
+            <img src={heroIMG} alt="news" className="h-[84px] w-[114px] rounded-lg" />
+            <p className=" font-mono text-sm hover:underline hover:cursor-pointer">
+              Charge Two Devices at the Same Time With This Magnetic Wireless Charging Dock
             </p>
           </div>
-          <div>
-            <img src="/" alt="news" />
-            <p>Charge Two Devices at the Same Time With This Magnetic Wireless Charging Dock
+          <div className="flex flex-col gap-2 border-2 border-[#366BD9] rounded-lg p-3 hover:bg-slate-500">
+            <img src={heroIMG} alt="news" className="h-[84px] w-[114px] rounded-lg" />
+            <p className=" font-mono">
+              Charge Two Devices at the Same Time With This Magnetic Wireless Charging Dock
             </p>
           </div>
         </div>
       </div>
-      <div>
-        <h2>Popular this week</h2>
-        <div>
-          <div>
-            <img src="" alt="" />
-            <div>
-              <div>
-                <span>Inovation</span>
-                <span>2 Hours Ago</span>
-              </div>
-              <p>7 Ways You Can Reduce Climate Change</p>
-            </div>
-          </div>
+      <div className="flex items-start flex-col text-white">
+        <h2 className="font-serif font-semibold text-2xl mb-4">Popular this week</h2>
+        <div className="flex flex-col gap-3">
+          {PopularNews.map((newsItem) => (
+            <PopularNewsCard
+              newsItem={newsItem}
+              key={newsItem.time}
+            />
+          ))}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
---
+++ src/components/PopularNewsCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PopularNewsCard = ({ newsItem }) => {
+  const {
+    image, slug, time, category,
+  } = newsItem;
+  return (
+    <div className="grid grid-cols-3 gap-3">
+      <img src={image} alt="news" className="h-[94px] w-[144px] rounded-lg" />
+      <div className="flex flex-col gap-2 col-span-2">
+        <div className="flex gap-4 font-thin italic">
+          <span>{category}</span>
+          <span>{time}</span>
+        </div>
+        <p className=" font-mono text-sm hover:underline hover:cursor-pointer">
+          {slug}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+PopularNewsCard.propTypes = {
+  newsItem: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
+
+export default PopularNewsCard;
